5b105fc28beac3ec4d8b2119c0701313d3c20e12
christamsnow <69160111+christamsnow@users.noreply.github.com>
2021-02-10 11:46:23 -0500

    getAllSubstrings passing all tests

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -5,7 +5,7 @@ package rocks.zipcode.io.quiz4.fundamentals;
  */
 public class StringEvaluator {
     /**
-     * method to add additional element to end of array
+     * created method to add additional element to end of array
      */
     public static String[] addElement(String[] array, String string){
         String[] newArray = new String[array.length + 1];
@@ -17,7 +17,7 @@ public class StringEvaluator {
     }
 
     /**
-     * method to find if element already exists in array
+     * created method to find if element already exists in array
      */
     public static Boolean elementExists(String[] array, String string){
         for (int i=0; i< array.length; i++){
@@ -30,9 +30,9 @@ public class StringEvaluator {
 
     public static String[] getAllSubstrings(String string) {
         String[] answer = new String[0];
-        for (int i=0; i<string.length(); i++){ //loop, adding each additional substring to answer[] if string.length > 1
+        for (int i=0; i<string.length(); i++){
             String sub = "";
-            for (int j=i; j<string.length(); j++){ //inner loop to break remainder of string into smaller strings starting at index i
+            for (int j=i; j<string.length(); j++){
                 sub = sub + String.valueOf(string.charAt(j));
                 if (!elementExists(answer, sub)){
                     answer = addElement(answer, sub);
